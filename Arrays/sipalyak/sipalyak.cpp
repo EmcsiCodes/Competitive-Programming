b97a06c1a872385f2e60e6f0200d7c1b623fd530
EmcsiCodes <125769287+EmcsiCodes@users.noreply.github.com>
2023-03-08 11:51:53 +0200

    Add files via upload

---
+++ Arrays/sipalyak/sipalyak.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define LL long long
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<LL> v;
+    for (int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    LL next = v[0] - 1;
+    LL currSum = v[0];
+    for (int i = 1; i < k; i++) {
+        currSum += v[i];
+        if (v[i] <= next) next--;
+        else next = v[i] - 1;
+    }
+    LL neededSum = ((next + k) * (next + k + 1) / 2) - (next * (next + 1) / 2);
+    LL best = neededSum - currSum;
+    LL l = 0, r = k;
+    //cout << best << " " << neededSum << "-" << currSum << endl;
+    while (r < v.size()) {
+        currSum = currSum - v[l] + v[r];
+        if (v[r] <= next) next--;
+        else next = v[r] - 1;
+        neededSum = ((next + k) * (next + k + 1) / 2) - (next * (next + 1) / 2);
+        //cout << best << " " << neededSum << "-" << currSum << endl;
+        best = min(best, neededSum - currSum);
+        r++;
+        l++;
+    }
+    cout << best;
+    return 0;
+}
