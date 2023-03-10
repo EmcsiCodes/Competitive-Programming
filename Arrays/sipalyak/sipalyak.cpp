52f3f63b2b9ccc805c9206c4112ec725e15a9fb0
EmcsiCodes <125769287+EmcsiCodes@users.noreply.github.com>
2023-03-10 12:58:37 +0200

    Update sipalyak.cpp

--- Arrays/sipalyak/sipalyak.cpp
+++ Arrays/sipalyak/sipalyak.cpp
@@ -15,15 +15,20 @@ int main()
     }
     LL next = v[0] - 1;
     LL currSum = v[0];
+    int ground = 0;
     for (int i = 1; i < k; i++) {
         currSum += v[i];
-        if (v[i] <= next) next--;
-        else next = v[i] - 1;
+        if (v[i] <= next) next--; //setben el kell menteni a abs(next - v[i])-t.
+        //ha elhagyjuk a groundot akkor uj ground = regi lepcso - min(abs ertekek)
+        else {
+            next = v[i] - 1;
+            ground = i;
+        }
     }
     LL neededSum = ((next + k) * (next + k + 1) / 2) - (next * (next + 1) / 2);
     LL best = neededSum - currSum;
     LL l = 0, r = k;
-    //cout << best << " " << neededSum << "-" << currSum << endl;
+    
     while (r < v.size()) {
         currSum = currSum - v[l] + v[r];
         if (v[r] <= next) next--;
